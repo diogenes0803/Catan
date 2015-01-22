780f7be08a6adcfc22cf2d711b95b50ffbe9d80c
yankeenate <yankeenate@yahoo.com>
2015-01-21 18:06:56 -0700

    Updated ServerPoller

--- java/src/client/communication/ServerPoller.java
+++ java/src/client/communication/ServerPoller.java
@@ -1,29 +1,37 @@
 package client.communication;
 
+/**
+ * Description: The Server Poller polls the server at regular intervals in order to update
+ * the current Model state.
+ * @author Nate Campbell
+ */
+
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class ServerPoller {
 	
 	private Timer timer;
-	private int versionID;
+	private ServerFacade facade;
 	
 	public ServerPoller() {
 		
 	}
 	
-	public boolean isDifferent() {
-		return false;
-	}
-	
-	public void updateModel() {
+	/**
+	 * @pre Nothing, the function is private.
+	 * @post Asks the server to update the current Model state. The server checks to see if anything actually needs to be updated.
+	 */
+	private void updateModel() {
 		
 	}
 }
 
 class CheckTask extends TimerTask {
-	/*
-	 * Will run isDifferent, if true it calls updateModel.
+	
+	/**
+	 * @pre Nothing.
+	 * @post Calls updateModel according to the scheduled interval.
 	 */
 	public void run() {
 		
