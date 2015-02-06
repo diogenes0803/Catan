fef3f868019d3319c6d2342713b8cd32ed0686d5
yankeenate <yankeenate@yahoo.com>
2015-02-06 16:10:39 -0700

    1)Created JUnit tests for CatanModel, ServerPoller, and ServerProxy

---
+++ java/src/test/CatanModelTest.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author campbeln
+ *
+ */
+public class CatanModelTest {
+
+	/**
+	 * Test method for {@link shared.models.CatanModel#getGameManager()}.
+	 */
+	@Test
+	public void testGetGameManager() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link shared.models.CatanModel#setGameManager(shared.models.GameManager)}.
+	 */
+	@Test
+	public void testSetGameManager() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link shared.models.CatanModel#getUserManager()}.
+	 */
+	@Test
+	public void testGetUserManager() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link shared.models.CatanModel#setUserManager(shared.models.UserManager)}.
+	 */
+	@Test
+	public void testSetUserManager() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
