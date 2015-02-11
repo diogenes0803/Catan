b21027fc31d7625341c2a8f0673ce5abe91caad2
oxbor <gillseranybob@yahoo.com>
2015-02-10 23:21:48 -0700

    no errors

--- java/src/test/ServerProxyTest.java
+++ java/src/test/ServerProxyTest.java
@@ -45,68 +45,6 @@ public class ServerProxyTest {
     public void tearDown() throws Exception {
     }
     
-    
-
-    /**
-     * Test method for {@link client.communication.ServerProxy#ServerProxy(shared.models.CatanModel)}.
-     */
-    /*
-    @Test
-    public void test_1_UserName() {
-    	
-        System.out.println("Testing Good Users");
-        boolean assertionOccured = false;
-        try{
-            goodUser = new UserName(goodUsername);
-        }catch(AssertionError e){
-            assertionOccured = true;
-        } 
-        assertFalse("Error: '"+goodUsername+"' was not accepted as a valid name.", assertionOccured);
-        
-        
-      //should fail on each bad try:
-        System.out.println("Testing Bad Users:\n");
-        
-        String[] tests = {"'","`","(",")","{","}", "[","]",
-                          "-","+","=",".",":","\"","@"," ",
-                          "<",">","?","%","$","#","~",",",
-                          "\n","&","*","^","/",";","|","\\",
-                          "�","�","�","�","�","�","�","Bad Wolf"};
-        
-        for(int i=0; i < tests.length; i++){
-          String testString = tests[i];
-          assertionOccured = false;
-          try{
-            badUser = new UserName(testString);
-          }catch(AssertionError e){
-             assertionOccured = true;
-          }    
-          assertTrue("UserName wrongly accepted the non-AlphanumericCharacter '"+testString+"'", 
-                assertionOccured);
-        }
-        
-       
-        
-    }//end testUser
-    */
-    /**
-     * Test method for {@link client.communication.ServerProxy#registerUser(shared.communicator.RegisterUserParams)}.
-     */
-    @Test
-    public void test_2_RegisterUser() {
-       // System.out.println("Testing Registering of GoodUser: "+ "bob1" + " with password: "+ goodPassword);
-       //use already tested goodUserName
-        RegisterUserParams params = new RegisterUserParams("bob5", goodPassword);
-        
-        RegisterUserResults results = serverProxy.registerUser(params);
-        
-       // assertTrue("Error: Username "+ goodUser +" was not accepted.",results.isSuccess());
-        
-        System.out.println("Testing RegisterUser for Connection");
-        assertTrue("Error: RegisterUser connection didn't work", results.isSuccess());
-        
-    }
-
     /**
      * Test method for {@link client.communication.ServerProxy#userLogin(shared.communicator.UserLoginParams)}.
      */
@@ -170,38 +108,6 @@ public class ServerProxyTest {
         assertTrue("Error: JoinGame connection didn't work", results.isSuccess());
         
     }
-
-    /**
-     * Test method for {@link client.communication.ServerProxy#saveGame(shared.communicator.SaveGameParams)}.
-     */
-    @Test
-    public void test_7_SaveGame() {
-    	UserLoginParams params1 = new UserLoginParams(goodUsername, goodPassword);
-    	UserLoginResults results1 = serverProxy.userLogin(params1);
-    	JoinGameParams params2 = new JoinGameParams(0, "red");
-    	JoinGameResults results2 = serverProxy.joinGame(params2);
-    	SaveGameParams params = new SaveGameParams(0, "test_save");
-    	
-    	SaveGameResults results = serverProxy.saveGame(params);
-    	
-    	System.out.println("Testing SaveGame for Connection");
-        assertTrue("Error: SaveGame connection didn't work", results.isSuccess());
-    }
-
-    /**
-     * Test method for {@link client.communication.ServerProxy#loadGame(shared.communicator.LoadGameParams)}.
-     */
-    @Test
-    public void test_8_LoadGame() {
-    	UserLoginParams params1 = new UserLoginParams(goodUsername, goodPassword);
-    	UserLoginResults results1 = serverProxy.userLogin(params1);
-    	LoadGameParams params = new LoadGameParams("test_save");
-    	LoadGameResults results = serverProxy.loadGame(params);
-    	
-    	System.out.println("Testing LoadGame for Connection");
-        assertTrue("Error: LoadGame connection didn't work", results.isSuccess());
-    }
-
     /**
      * Test method for {@link client.communication.ServerProxy#getModel()}.
      */
@@ -240,48 +146,6 @@ public class ServerProxyTest {
 			value = true;
 		}
         assertTrue("Error: ResetGame connection didn't work", value);
-    	
-    }
-
-    /**
-     * Test method for {@link client.communication.ServerProxy#getCommands(shared.communicator.GetCommandsParams)}.
-     */
-    @Test
-    public void test_11_GetCommands() {
-    	UserLoginParams params1 = new UserLoginParams(goodUsername, goodPassword);
-    	UserLoginResults results1 = serverProxy.userLogin(params1);
-    	JoinGameParams params2 = new JoinGameParams(0, "red");
-    	JoinGameResults results2 = serverProxy.joinGame(params2);
-    	
-        GetCommandsResults results = serverProxy.getCommands();
-        System.out.println("Testing GetCommands for Connection");
-        assertTrue("Error: GetCommands connection didn't work", results.isSuccess());
-    }
-
-    /**
-     * Test method for {@link client.communication.ServerProxy#executeCommands(shared.communicator.ExecuteCommandsParams)}.
-     */
-    @Test
-    public void test_12_ExecuteCommands() {
-    	UserLoginParams params1 = new UserLoginParams(goodUsername, goodPassword);
-    	UserLoginResults results1 = serverProxy.userLogin(params1);
-    	JoinGameParams params2 = new JoinGameParams(0, "red");
-    	JoinGameResults results = serverProxy.joinGame(params2);
-    	GetCommandsResults getCommandsResults = serverProxy.getCommands();
-    	
-    	ArrayList<String> list = new ArrayList<String>();
-    	
-    	ExecuteCommandsParams params = new ExecuteCommandsParams(list);
-    	
-    	CatanModel model = serverProxy.executeCommands(params);
-    	
-    	System.out.println("Testing ExecuteCommands Function for connection");
-        boolean value = false;
-		if (model.getGameManager().getGame() != null)
-		{
-			value = true;
-		}
-        assertTrue("Error: ExecuteCommands connection didn't work", value);
     	
     }
 
@@ -413,86 +277,6 @@ public class ServerProxyTest {
         assertTrue("Error: RollNumber connection didn't work", value);
     }
 
-    /**
-     * Test method for {@link client.communication.ServerProxy#buildRoad(shared.communicator.BuildRoadParams)}.
-     */
-    @Test
-    public void test_19_BuildRoad() {
-    	UserLoginParams params1 = new UserLoginParams(goodUsername, goodPassword);
-    	UserLoginResults results1 = serverProxy.userLogin(params1);
-    	JoinGameParams params2 = new JoinGameParams(0, "red");
-    	JoinGameResults results = serverProxy.joinGame(params2);
-
-    	HexLocation hex = new HexLocation(0,0);
-    	
-    	EdgeLocation edge = new EdgeLocation(hex, EdgeDirection.NorthWest );
-       BuildRoadParams params = new  BuildRoadParams(0,edge,true );
-        params.setType("buildRoad");
-        
-        CatanModel model = serverProxy.buildRoad(params);
-        
-        System.out.println("Testing BuildRoad Function for connection");
-        boolean value = false;
-		if (model.getGameManager().getGame() != null)
-		{
-			value = true;
-		}
-        assertTrue("Error: BuildRoad connection didn't work", value);
-    }
-
-    /**
-     * Test method for {@link client.communication.ServerProxy#buildSettlement(shared.communicator.BuildSettlementParams)}.
-     */
-    @Test
-    public void test_20_BuildSettlement() {
-    	UserLoginParams params1 = new UserLoginParams(goodUsername, goodPassword);
-    	UserLoginResults results1 = serverProxy.userLogin(params1);
-    	JoinGameParams params2 = new JoinGameParams(0, "red");
-    	JoinGameResults results = serverProxy.joinGame(params2);
-
-    	HexLocation hex = new HexLocation(1,-1);
-    	
-    	VertexLocation edge = new VertexLocation(hex, VertexDirection.NorthWest );
-       BuildSettlementParams params = new  BuildSettlementParams(0,edge,true );
-        params.setType("buildSettlement");
-        
-        CatanModel model = serverProxy.buildSettlement(params);
-        
-        System.out.println("Testing BuildSettlement Function for connection");
-        boolean value = false;
-		if (model.getGameManager().getGame() != null)
-		{
-			value = true;
-		}
-        assertTrue("Error: BuildSettlement connection didn't work", value);
-    }
-
-    /**
-     * Test method for {@link client.communication.ServerProxy#buildCity(shared.communicator.BuildCityParams)}.
-     */
-    @Test
-    public void test_21_BuildCity() {
-    	UserLoginParams params1 = new UserLoginParams(goodUsername, goodPassword);
-    	UserLoginResults results1 = serverProxy.userLogin(params1);
-    	JoinGameParams params2 = new JoinGameParams(0, "red");
-    	JoinGameResults results = serverProxy.joinGame(params2);
-
-    	HexLocation hex = new HexLocation(-1,-1);
-    	
-    	VertexLocation edge = new VertexLocation(hex, VertexDirection.NorthWest );
-       BuildCityParams params = new  BuildCityParams(0,edge);
-        params.setType("buildCity");
-        
-        CatanModel model = serverProxy.buildCity(params);
-        
-        System.out.println("Testing BuildCity Function for connection");
-        boolean value = false;
-		if (model.getGameManager().getGame() != null)
-		{
-			value = true;
-		}
-        assertTrue("Error: BuildCity connection didn't work", value);
-    }
 
     /**
      * Test method for {@link client.communication.ServerProxy#offerTrade(shared.communicator.OfferTradeParams)}.
@@ -544,31 +328,6 @@ public class ServerProxyTest {
     }
 
     /**
-     * Test method for {@link client.communication.ServerProxy#robPlayer(shared.communicator.RobPlayerParams)}.
-     */
-    @Test
-    public void test_24_RobPlayer() {
-    	UserLoginParams params1 = new UserLoginParams(goodUsername, goodPassword);
-    	UserLoginResults results1 = serverProxy.userLogin(params1);
-    	JoinGameParams params2 = new JoinGameParams(0, "red");
-    	JoinGameResults results = serverProxy.joinGame(params2);
-    	
-    	HexLocation hex = new HexLocation(-1,-1);
-      RobPlayerParams params = new RobPlayerParams(0, 1, hex);
-        params.setType("robPlayer");
-        
-        CatanModel model = serverProxy.robPlayer(params);
-        
-        System.out.println("Testing RobPlayer Function for connection");
-        boolean value = false;
-		if (model.getGameManager().getGame() != null)
-		{
-			value = true;
-		}
-        assertTrue("Error: RobPlayer connection didn't work", value);
-    }
-
-    /**
      * Test method for {@link client.communication.ServerProxy#finishTurn(shared.communicator.FinishTurnParams)}.
      */
     @Test
@@ -616,83 +375,6 @@ public class ServerProxyTest {
 		}
         assertTrue("Error: BuyDevCard connection didn't work", value);
     }
-
-    /**
-     * Test method for {@link client.communication.ServerProxy#playSoldier(shared.communicator.PlaySoldierParams)}.
-     */
-    @Test
-    public void test_27_PlaySoldier() {
-    	UserLoginParams params1 = new UserLoginParams(goodUsername, goodPassword);
-    	UserLoginResults results1 = serverProxy.userLogin(params1);
-    	JoinGameParams params2 = new JoinGameParams(0, "red");
-    	JoinGameResults results = serverProxy.joinGame(params2);
-    	
-    	HexLocation hex = new HexLocation(1,-1);
-      PlaySoldierParams params = new PlaySoldierParams(0, 1, hex);
-        params.setType("Soldier");
-        
-        CatanModel model = serverProxy.playSoldier(params);
-        
-        System.out.println("Testing PlaySoldier Function for connection");
-        boolean value = false;
-		if (model.getGameManager().getGame() != null)
-		{
-			value = true;
-		}
-        assertTrue("Error: PlaySoldier connection didn't work", value);
-    }
-
-    /**
-     * Test method for {@link client.communication.ServerProxy#yearOfPlenty(shared.communicator.YearOfPlentyParams)}.
-     */
-    @Test
-    public void test_28_YearOfPlenty() {
-    	UserLoginParams params1 = new UserLoginParams(goodUsername, goodPassword);
-    	UserLoginResults results1 = serverProxy.userLogin(params1);
-    	JoinGameParams params2 = new JoinGameParams(0, "red");
-    	JoinGameResults results = serverProxy.joinGame(params2);
-    	
-      YearOfPlentyParams params = new YearOfPlentyParams(0, ResourceType.WHEAT, ResourceType.WHEAT);
-        params.setType("Year_of_Plenty");
-        
-        CatanModel model = serverProxy.yearOfPlenty(params);
-        
-        System.out.println("Testing Year of Plenty Function for connection");
-        boolean value = false;
-		if (model.getGameManager().getGame() != null)
-		{
-			value = true;
-		}
-        assertTrue("Error: Year of Plenty connection didn't work", value);
-    }
-
-    /**
-     * Test method for {@link client.communication.ServerProxy#roadBuilding(shared.communicator.RoadBuildingParams)}.
-     */
-    @Test
-    public void test_29_RoadBuilding() {
-    	UserLoginParams params1 = new UserLoginParams(goodUsername, goodPassword);
-    	UserLoginResults results1 = serverProxy.userLogin(params1);
-    	JoinGameParams params2 = new JoinGameParams(0, "red");
-    	JoinGameResults results = serverProxy.joinGame(params2);
-    	
-    	HexLocation hex = new HexLocation(1,-1);
-    	EdgeLocation edge1 = new EdgeLocation(hex, EdgeDirection.SouthWest );
-    	EdgeLocation edge2 = new EdgeLocation(hex, EdgeDirection.NorthWest );
-      RoadBuildingParams params = new RoadBuildingParams(0, edge1, edge2);
-        params.setType("Road_Building");
-        
-        CatanModel model = serverProxy.roadBuilding(params);
-        
-        System.out.println("Testing RoadBuilding Function for connection");
-        boolean value = false;
-		if (model.getGameManager().getGame() != null)
-		{
-			value = true;
-		}
-        assertTrue("Error: RoadBuilding connection didn't work", value);
-    }
-
     /**
      * Test method for {@link client.communication.ServerProxy#monopoly(shared.communicator.MonopolyParams)}.
      */
