c06c33ba8a87cefc09fba5ef5b7fe0473945727e
Hojune <soy0803@gmail.com>
2015-03-02 01:06:08 -0700

    Game Log Controller updated

--- java/src/client/communication/GameHistoryController.java
+++ java/src/client/communication/GameHistoryController.java
@@ -5,6 +5,9 @@ import java.util.List;
 import java.util.Observable;
 
 import shared.definitions.CatanColor;
+import shared.models.CatanModel;
+import shared.models.Game;
+import shared.models.MessageLine;
 import client.base.Controller;
 
 
@@ -30,26 +33,30 @@ public class GameHistoryController extends Controller implements IGameHistoryCon
 		
 		//<temp>
 		
-		List<LogEntry> entries = new ArrayList<LogEntry>();
-		entries.add(new LogEntry(CatanColor.BROWN, "This is a brown message"));
-		entries.add(new LogEntry(CatanColor.ORANGE, "This is an orange message ss x y z w.  This is an orange message.  This is an orange message.  This is an orange message."));
-		entries.add(new LogEntry(CatanColor.BROWN, "This is a brown message"));
-		entries.add(new LogEntry(CatanColor.ORANGE, "This is an orange message ss x y z w.  This is an orange message.  This is an orange message.  This is an orange message."));
-		entries.add(new LogEntry(CatanColor.BROWN, "This is a brown message"));
-		entries.add(new LogEntry(CatanColor.ORANGE, "This is an orange message ss x y z w.  This is an orange message.  This is an orange message.  This is an orange message."));
-		entries.add(new LogEntry(CatanColor.BROWN, "This is a brown message"));
-		entries.add(new LogEntry(CatanColor.ORANGE, "This is an orange message ss x y z w.  This is an orange message.  This is an orange message.  This is an orange message."));
-		
-		getView().setEntries(entries);
+		Game game = CatanModel.getInstance().getGameManager().getGame();
+		updateFromModel(game);
 	
 		//</temp>
 	}
 
 	@Override
 	public void update(Observable o, Object arg) {
-		// TODO Auto-generated method stub
-		
+		Game currentGame = CatanModel.getInstance().getGameManager().getGame();
+		Game updatedGame = (Game)arg;
+		if(updatedGame.getLogs().size() - currentGame.getLogs().size() != 0) {
+			updateFromModel(updatedGame);
+		}
 	}
 	
+	private void updateFromModel(Game game) {
+		List<LogEntry> entries = new ArrayList<LogEntry>();
+		List<MessageLine> logs = game.getLogs();
+		for(MessageLine log : logs) {
+			CatanColor playerColor = game.getPlayerColorByPlayerName(log.getSource());
+			entries.add(new LogEntry(playerColor, log.getMessage()));
+		}
+		
+		getView().setEntries(entries);
+	}
 }
 
