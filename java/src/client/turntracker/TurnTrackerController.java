b7bf46569f2711ea170f0d7fb567713ddd31d23a
nickdollar <ivelacc@gmail.com>
2015-03-03 00:41:36 -0700

    Turn Tracker almost Done

--- java/src/client/turntracker/TurnTrackerController.java
+++ java/src/client/turntracker/TurnTrackerController.java
@@ -1,7 +1,10 @@
 package client.turntracker;
 
 import client.base.Controller;
+import client.communication.ServerProxy;
 import shared.definitions.CatanColor;
+import shared.models.Game;
+import shared.models.TurnTracker;
 
 import java.util.Observable;
 
@@ -37,8 +40,64 @@ public class TurnTrackerController extends Controller implements ITurnTrackerCon
 
     @Override
     public void update(Observable o, Object arg) {
-        // TODO Auto-generated method stub
-
+    	Game thisGame = (Game)arg;
+    	getView().setLocalPlayerColor(thisGame.getPlayers()[ServerProxy.getInstance().getlocalPlayer().getPlayerIndex()].getColor());
+    	
+    	boolean largestArmy = false;
+    	boolean longestRoad = false;
+    	
+    	//Need to know when the TurnTacker Initialize
+    	if(thisGame.getTurnTracker()!=null)
+    	{
+    		
+    		getView().initializePlayer(thisGame.getTurnTracker().getCurrentTurn(), thisGame.getPlayers()[thisGame.getTurnTracker().getCurrentTurn()].getName(),
+    				thisGame.getPlayers()[thisGame.getTurnTracker().getCurrentTurn()].getColor());
+    		
+    		
+    		
+    		
+    		
+        	if(thisGame.getTurnTracker().getCurrentTurn()==thisGame.getTurnTracker().getLargestArmy())
+        	{
+        		largestArmy = true;
+        	}
+        	
+        	if(thisGame.getTurnTracker().getCurrentTurn()==thisGame.getTurnTracker().getLongestRoad())
+        	{
+        		longestRoad = true;
+        	}
+        	System.out.println("AAAAAAAAAAAAAAAAAaa" + thisGame.getTurnTracker().getCurrentTurn());
+        	getView().updatePlayer(
+        			thisGame.getTurnTracker().getCurrentTurn(), 
+        			thisGame.getPlayers()[thisGame.getTurnTracker().getCurrentTurn()].getVictoryPoint(), 
+        			true, 
+        			largestArmy,
+        			longestRoad);
+        
+        	
+        	
+        /**
+       	  * I can only find two messages
+       	  * "Waiting for others player"
+       	  * "Finish Turn"
+       	  * Finish Turn
+       	  * getView().updateGameState("Game State Button", true);
+       	  * 
+       	  **/
+        	
+        	if(ServerProxy.getInstance().getlocalPlayer().getPlayerIndex()!=thisGame.getTurnTracker().getCurrentTurn())
+        		
+        	{
+        		getView().updateGameState("Waiting for others players", true);
+        	}
+        	else
+        	{
+        		getView().updateGameState("End the turn", true);
+        	}
+    	}
+    	
+    	 
+    			
     }
 
 }
