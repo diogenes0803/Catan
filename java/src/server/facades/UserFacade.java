cf6362bb55eb7316fa09a7ff905356cb0ea3d364
oxbor <gillseranybob@yahoo.com>
2015-03-12 18:22:38 -0600

    userFacade

--- java/src/server/facades/UserFacade.java
+++ java/src/server/facades/UserFacade.java
@@ -3,10 +3,35 @@
  */
 package server.facades;
 
+import shared.communicator.RegisterUserParams;
+import shared.communicator.RegisterUserResults;
+import shared.communicator.UserLoginParams;
+import shared.communicator.UserLoginResults;
+
 /**
  * @author campbeln
  *
  */
 public class UserFacade implements Facade {
+	
+	/**
+	 * 
+	 * @param params
+	 * @return
+	 */
+	 public UserLoginResults userLogin(UserLoginParams params) {
+		 UserLoginResults result = new UserLoginResults();
+		 return result;
+	 }
+	 
+	 /**
+	  * 
+	  * @param params
+	  * @return
+	  */
+	 public RegisterUserResults registerUser(RegisterUserParams params) {
+	        RegisterUserResults result = new RegisterUserResults();
+	        return result;
+	 }
 
 }
