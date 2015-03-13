3c65a80fc0f938f63f20b1b01f2d187c6c0ed086
yankeenate <yankeenate@yahoo.com>
2015-03-12 19:10:46 -0600

    Javdocs and method stubs made for MovesFacade

--- java/src/server/facades/MovesFacade.java
+++ java/src/server/facades/MovesFacade.java
@@ -3,10 +3,165 @@
  */
 package server.facades;
 
+import shared.communicator.*;
+import shared.models.CatanModel;
+
 /**
+ * Facade containing the server's implementation of all Move methods
  * @author campbeln
- *
  */
 public class MovesFacade implements Facade {
+	
+	/**
+	 * @param params Data holder containing schema required for this function
+	 * @return CatanModel object containing the updated Game model
+	 */
+	public CatanModel sendChat(SendChatParams params) {
+		//TODO sendChat method
+		return null;
+	}
+	
+	/**
+	 * @param params Data holder containing schema required for this function
+	 * @return CatanModel object containing the updated Game model
+	 */
+	public CatanModel rollNumber(RollNumberParams params) {
+		//TODO rollNumber method
+		return null;
+	}
+	
+	/**
+	 * @param params Data holder containing schema required for this function
+	 * @return CatanModel object containing the updated Game model
+	 */
+	public CatanModel robPlayer(RobPlayerParams params) {
+		//TODO robPlayer method
+		return null;
+	}
+	
+	/**
+	 * @param params Data holder containing schema required for this function
+	 * @return CatanModel object containing the updated Game model
+	 */
+	public CatanModel finishTurn(FinishTurnParams params) {
+		//TODO finishTurn method
+		return null;
+	}
+	
+	/**
+	 * @param params Data holder containing schema required for this function
+	 * @return CatanModel object containing the updated Game model
+	 */
+	public CatanModel buyDevCard(BuyDevCardParams params) {
+		//TODO buyDevCard method
+		return null;
+	}
+	
+	/**
+	 * @param params Data holder containing schema required for this function
+	 * @return CatanModel object containing the updated Game model
+	 */
+	public CatanModel yearOfPlenty(YearOfPlentyParams params) {
+		//TODO yearOfPlenty method
+		return null;
+	}
+	
+	/**
+	 * @param params Data holder containing schema required for this function
+	 * @return CatanModel object containing the updated Game model
+	 */
+	public CatanModel roadBuilding(RoadBuildingParams params) {
+		//TODO roadBuilding method
+		return null;
+	}
+	
+	/**
+	 * @param params Data holder containing schema required for this function
+	 * @return CatanModel object containing the updated Game model
+	 */
+	public CatanModel playSoldier(PlaySoldierParams params) {
+		//TODO playSoldier method
+		return null;
+	}
 
+	/**
+	 * @param params Data holder containing schema required for this function
+	 * @return CatanModel object containing the updated Game model
+	 */
+	public CatanModel monopoly(MonopolyParams params) {
+		//TODO monopoly method
+		return null;
+	}
+	
+	 /**
+	 * @param params Data holder containing schema required for this function
+	 * @return CatanModel object containing the updated Game model
+	 */
+	public CatanModel monument(MonumentParams params) {
+		 //TODO monument method
+		 return null;
+	 }
+	 
+	 /**
+	 * @param params Data holder containing schema required for this function
+	 * @return CatanModel object containing the updated Game model
+	 */
+	public CatanModel buildRoad(BuildRoadParams params) {
+		 //TODO buildRoad method
+		return null;
+	 }
+	 
+	 /**
+	 * @param params Data holder containing schema required for this function
+	 * @return CatanModel object containing the updated Game model
+	 */
+	public CatanModel buildSettlement(BuildSettlementParams params) {
+		 //TODO buildSettlement method
+		 return null;
+	 }
+	 
+	 /**
+	 * @param params Data holder containing schema required for this function
+	 * @return CatanModel object containing the updated Game model
+	 */
+	public CatanModel buildCity(BuildCityParams params) {
+		 //TODO buildCity method
+		 return null;
+	 }
+	 
+	 /**
+	 * @param params Data holder containing schema required for this function
+	 * @return CatanModel object containing the updated Game model
+	 */
+	public CatanModel offerTrade(OfferTradeParams params) {
+		 //TODO offerTrade method
+		 return null;
+	 }
+	 
+	 /**
+	 * @param params Data holder containing schema required for this function
+	 * @return CatanModel object containing the updated Game model
+	 */
+	public CatanModel acceptTrade(AcceptTradeParams params) {
+		 //TODO acceptTrade method
+		 return null;
+	 }
+	 
+	 /**
+	 * @param params Data holder containing schema required for this function
+	 * @return CatanModel object containing the updated Game model
+	 */
+	public CatanModel maritimeTrade(MaritimeTradeParams params) {
+		 //TODO maritimeTrade method
+		 return null;
+	 }
+	 
+	 /**
+	 * @param params Data holder containing schema required for this function
+	 * @return CatanModel object containing the updated Game model
+	 */
+	public CatanModel discardCards(DiscardCardsParams params) {
+		 //TODO discardCards method
+		 return null;
+	 }
 }
