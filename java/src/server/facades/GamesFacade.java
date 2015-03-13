fad31f5f14070708125d0b5a4297825cef87a005
Hojune <soy0803@gmail.com>
2015-03-13 01:47:29 -0600

    GamesFacade

--- java/src/server/facades/GamesFacade.java
+++ java/src/server/facades/GamesFacade.java
@@ -24,21 +24,41 @@ public GamesFacade(){}
 		
 	}
 	
+	/**
+	 * Takes Create Game Params and create a new game and save it in the server
+	 * @param thisParams
+	 * @return CreateGameResults
+	 */
 	public CreateGameResults create(CreateGameParams thisParams) {
 		CreateGameResults thisResult = new CreateGameResults();
 		return thisResult;
 	}
 	
+	/**
+	 * Takes JoinGameParams and put joined user information in to the game
+	 * @param thisParams
+	 * @return JoinGameResults
+	 */
 	public JoinGameResults join(JoinGameParams thisParams) {
 		JoinGameResults thisResult = new JoinGameResults();
 		return thisResult;
 	}
 	
+	/**
+	 * Takes SaveGameParams and save current game model
+	 * @param thisParams
+	 * @return SaveGameResults
+	 */
 	public SaveGameResults save(SaveGameParams thisParams) {
 		SaveGameResults thisResult = new SaveGameResults();
 		return thisResult;
 	}
 	
+	/**
+	 * Takes LoadGameResults and load saved game model
+	 * @param thisParams
+	 * @return LoadGameResults
+	 */
 	public LoadGameResults load(LoadGameParams thisParams) {
 		LoadGameResults thisResult = new LoadGameResults();
 		return thisResult;
