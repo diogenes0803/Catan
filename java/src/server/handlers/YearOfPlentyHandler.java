5dc877fd8a4beb6864c96c0d0c2ea3fd9c0c521e
yankeenate <yankeenate@yahoo.com>
2015-03-25 15:46:34 -0600

    Handlers for several move methods, probably not functional yet

---
+++ java/src/server/handlers/YearOfPlentyHandler.java
@@ -0,0 +1,70 @@
+/**
+ * 
+ */
+package server.handlers;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import server.facades.MovesFacade;
+import shared.communicator.YearOfPlentyParams;
+import shared.models.CatanModel;
+
+import com.google.gson.Gson;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+/**
+ * @author campbeln
+ *
+ */
+public class YearOfPlentyHandler implements HttpHandler {
+
+	private MovesFacade movesFacade = new MovesFacade();
+	
+	/* (non-Javadoc)
+	 * @see com.sun.net.httpserver.HttpHandler#handle(com.sun.net.httpserver.HttpExchange)
+	 */
+	@Override
+	public void handle(HttpExchange exchange) throws IOException {
+		// TODO Auto-generated method stub
+
+		Gson gson = new Gson(); 
+		
+		String qry;
+		String encoding = "ISO-8859-1";
+		InputStream in = exchange.getRequestBody();
+		try {
+		    ByteArrayOutputStream out = new ByteArrayOutputStream();
+		    byte buf[] = new byte[4096];
+		    for (int n = in.read(buf); n > 0; n = in.read(buf)) {
+		        out.write(buf, 0, n);
+		    }
+		    qry = new String(out.toByteArray(), encoding);
+		} finally {
+		    in.close();
+		}
+		
+		YearOfPlentyParams params = gson.fromJson(qry, YearOfPlentyParams.class);
+		CatanModel result = movesFacade.yearOfPlenty(params);
+		String resultGson = gson.toJson(result);
+		
+		OutputStream out = exchange.getResponseBody();
+		exchange.getResponseHeaders().add("Content-Type", "text/html");
+		String body = "";
+		if(result != null) {  //Facade passes back null if command is invalid
+			body = resultGson;
+			exchange.sendResponseHeaders(200, body.length());
+		}
+		else {
+			body = "Invalid Command";
+			exchange.sendResponseHeaders(400, body.length());
+		}
+		out.write(body.getBytes());
+		out.flush();
+		out.close();
+	}
+
+}
