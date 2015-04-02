50c53bba1c1a0ccfb1a917920f7550b496a40180
devyhj <soy0803@gmail.com>
2015-04-01 18:39:35 -0600

    Initializing Map from server

--- java/src/server/model/ServerModel.java
+++ java/src/server/model/ServerModel.java
@@ -1,15 +1,68 @@
 package server.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import shared.data.GameInfo;
 import shared.data.PlayerInfo;
 import shared.definitions.CatanColor;
+import shared.definitions.DevCardType;
+import shared.definitions.HexType;
+import shared.definitions.ResourceType;
+import shared.models.Bank;
+import shared.models.CatanMap;
+import shared.models.DevCard;
 import shared.models.Game;
+import shared.models.HexTile;
 import shared.models.Player;
+import shared.models.ResCard;
 
 public class ServerModel extends Game {
 	public ServerModel() {
 		super();
+		initialization();
 	}
+	private void initialization() {
+		this.setBank(createBank());
+		
+	}
+	
+	private Bank createBank() {
+		Bank newBank = new Bank();
+		List<DevCard> devCards = new ArrayList<DevCard>();
+		List<ResCard> resCards = new ArrayList<ResCard>();
+		devCards.add(new DevCard(DevCardType.YEAR_OF_PLENTY, false));
+		devCards.add(new DevCard(DevCardType.YEAR_OF_PLENTY, false));
+		devCards.add(new DevCard(DevCardType.MONOPOLY, false));
+		devCards.add(new DevCard(DevCardType.MONOPOLY, false));
+		for(int i = 0; i < 14; i++) {
+			devCards.add(new DevCard(DevCardType.SOLDIER, false));
+		}
+		devCards.add(new DevCard(DevCardType.ROAD_BUILD, false));
+		devCards.add(new DevCard(DevCardType.ROAD_BUILD, false));
+		for(int i = 0; i < 5; i ++) {
+			devCards.add(new DevCard(DevCardType.MONUMENT, false));
+		}
+		for(int i=0; i < 24; i++) {
+			resCards.add(new ResCard(ResourceType.BRICK));
+			resCards.add(new ResCard(ResourceType.WOOD));
+			resCards.add(new ResCard(ResourceType.SHEEP));
+			resCards.add(new ResCard(ResourceType.WHEAT));
+			resCards.add(new ResCard(ResourceType.ORE));
+		}
+		newBank.setDevCards(devCards);
+		newBank.setResCards(resCards);
+		return newBank;
+	}
+	
+	private CatanMap createMap() {
+		CatanMap newMap = new CatanMap();
+		HexTile[][] newHexArray = new HexTile[7][7];
+		newHexArray[0+3][-2+3] = new HexTile(0, -2, HexType.DESERT, 0);
+		newHexArray[0+3][-2+3] = new HexTile(0, -2, HexType.DESERT, 0);
+		return newMap;
+	}
+	
 	public GameInfo toGameInfo() {
 		GameInfo thisGameInfo = new GameInfo();
 		thisGameInfo.setId(this.getGameId());
